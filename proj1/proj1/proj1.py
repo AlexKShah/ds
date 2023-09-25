eddf6d0ea155942d556980ee97a8cc56d54b236b
AlexKShah <AlexKShah@users.noreply.github.com>
2023-09-25 15:06:39 -0400

    reupload fixed proj1

--- proj1/proj1/proj1.py
+++ proj1/proj1/proj1.py
@@ -16,34 +16,41 @@ def process_files(input_file: TextIO, output_file: TextIO) -> None:
             # Remove any remaining internal whitespace and convert to postfix
             cleaned_line = ''.join(cleaned_line.split())
             ans = convert_prefix_to_postfix(cleaned_line)
-            output_file.write(ans + '\n')
+            for an in ans:
+                output_file.write(an)
+            output_file.write('\n')
         except ValueError as e:
             # Handle invalid expressions
             print(f'Error: {e}', file=stderr)
 
 
 #  Check operator
+
 def isOperator(c: str) -> bool:
-    if c in ['+', '_', '*', '/', '^', '$']:
-        return True
-    else:
-        return False
+    return c in ['+', '-', '*', '/', '^', '$']
 
 
 def convert_prefix_to_postfix(pre: str) -> str:
     s = MyStack()
+    # Reverse the prefix expression
+    reversed_expression = pre[::-1]
 
-    # reverse input string
-    pre = pre[::-1]
-
-    for i in pre:
-        if isOperator(i):
-            a = s.pop()
-            b = s.pop()
-            s.push(str(a + b + i))
+    # Iterate through the reversed expression
+    for char in reversed_expression:
+        if not isOperator(char):
+            # Operand: Push it onto the stack
+            s.push(char)
         else:
-            s.push(i)
-    ss = []
+            # Operator: Pop two operands from the stack and create a new operand
+            operand1 = s.pop()
+            operand2 = s.pop()
+            if operand1 is not None and operand2 is not None:
+                new_operand = operand1 + operand2 + char
+                s.push(new_operand)
+            else:
+                raise ValueError("Not enough operands in the expression: " + str(pre) + " operand1: " + str(operand1) + " operand2: " + str(operand2) + " operator: " + str(char))
+
+    postfix_expression = ""
     while not s.is_empty():
-        ss += s.pop()
-        return str(*ss)
+        postfix_expression += s.pop()  # Pop and add items from the stack
+    return postfix_expression
