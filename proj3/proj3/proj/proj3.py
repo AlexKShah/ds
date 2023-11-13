1dadbc02ff07e833b1d67a5972d25312a617c69d
AlexKShah <AlexKShah@users.noreply.github.com>
2023-11-13 13:12:00 -0500

    proj3 fix error in tree logic, using preorder and special alphabetizing

--- proj3/proj3/proj/proj3.py
+++ proj3/proj3/proj/proj3.py
@@ -4,53 +4,16 @@ This program receives a frequency table, and encoded or cleartext text to be use
 __author__ = Alex Shah
 __version__ = proj3
 """
+
 from proj.mystack import MyPriorityQueue, HuffmanNode
 
 
-def test_case(huffman_codes, huffman_root):
-    # DEBUG
-    given_codes = {
-        'E': '010',
-        'S': '1110',
-        'T': '1001',
-        'R': '1000',
-        'N': '0111',
-        'W': '0011',
-        'L': '0010',
-        'A': '11111',
-        'M': '0000',
-        'O': '11110',
-        'H': '11011',
-        'C': '11010',
-        'I': '11001',
-        'B': '11000',
-        'U': '10111',
-        'G': '10110',
-        'P': '10100',
-        'F': '01101',
-        'D': '01100',
-        'K': '00011',
-        'Y': '101011',
-        'V': '000101',
-        'J': '000100',
-        'Z': '1010101',
-        'X': '10101001',
-        'Q': '10101000'
-    }
-
-    print("Comparing generated Huffman codes with given codes...")
-    for char, given_code in given_codes.items():
-        generated_code = huffman_codes.get(char)
-        if generated_code != given_code:
-            print(f"Mismatch for '{char}': Generated Code: {generated_code}, Given Code: {given_code}")
-        else:
-            print(f"Match for '{char}': Code: {generated_code}")
-
+def test_case(huffman_codes, huffman_root, output_file):
     # Encode and Decode "Hello World" to test functionality
     original_text = "Hello World".upper()
     original_encoded = "1101101000010001111100011111101000000101100"
-    encoded_text = huffman_encoding(original_text, huffman_codes)
-    decoded_text = huffman_decoding(original_encoded, huffman_root)
+    encoded_text = huffman_encoding(original_text, huffman_codes, output_file)
+    decoded_text = huffman_decoding(original_encoded, huffman_root, output_file)
 
     print("Original Text:\t", original_text)
     print("Encoded Text:\t", encoded_text)
@@ -66,24 +29,28 @@ def huffman_process_files(freq_file, encoded_file, clear_file, output_file):
         frequency_table[char] = int(freq)
 
     huffman_root = build_huffman_tree(frequency_table)
-    huffman_codes = generate_codes(huffman_root)
+    huffman_codes = get_huffman_codes(huffman_root)
 
+    output_file.write("####Encoded Lines:\n\n")
     for clear_line in clear_file:
-        encoded_line = huffman_encoding(clear_line.strip(), huffman_codes)
+        encoded_line = huffman_encoding(clear_line.strip(), huffman_codes, output_file)
         output_file.write(encoded_line + "\n")
+
+    output_file.write("\n\n####Decoded Lines:\n\n")
     for encoded_line in encoded_file:
-        decoded_text = huffman_decoding(encoded_line.strip(), huffman_root)
+        decoded_text = huffman_decoding(encoded_line.strip(), huffman_root, output_file)
         output_file.write(decoded_text + "\n")
+
     # DEBUG
-    test_case(huffman_codes, huffman_root)
+    test_case(huffman_codes, huffman_root, output_file)
 
 
 def build_huffman_tree(frequency_table):
     priority_queue = MyPriorityQueue()
     for char, freq in frequency_table.items():
-        priority_queue.push(HuffmanNode(char.upper(), freq))  # Consistent upper case
+        priority_queue.push(HuffmanNode(char, freq))
 
-    while priority_queue.size() > 1:
+    while len(priority_queue) > 1:
         left = priority_queue.pop()
         right = priority_queue.pop()
 
@@ -96,43 +63,62 @@ def build_huffman_tree(frequency_table):
     return priority_queue.pop()
 
 
-def generate_codes(node, prefix="", code_map={}):
-    if node is None:
-        return code_map
-
-    if node.char is not None:
-        code_map[node.char] = prefix
-
-    generate_codes(node.left, prefix + "0", code_map)
-    generate_codes(node.right, prefix + "1", code_map)
-
-    return code_map
+def assign_codes_preorder(node, code, codes):
+    if node is not None:
+        if node.char is not None:
+            codes[node.char] = code
+        assign_codes_preorder(node.left, code + "0", codes)
+        assign_codes_preorder(node.right, code + "1", codes)
 
 
-def huffman_encoding(text, code_map):
+def get_huffman_codes(root):
+    codes = {}
+    assign_codes_preorder(root, "", codes)
+    return codes
+
+
+def huffman_encoding(text, code_map, output_file):
     encoded_text = []
+    skipped_chars = set()
+
     for char in text.upper():
         if char in code_map:
             encoded_char = code_map[char]
             encoded_text.append(encoded_char)
+        else:
+            skipped_chars.add(char)
+
     final_encoded_string = ''.join(encoded_text)
-    # print(f"Final Encoded String: {final_encoded_string}")
+
+    if skipped_chars:
+        output_file.write("Skipped characters during encoding: " + ", ".join(skipped_chars) + "\n")
+
     return final_encoded_string
 
 
-def huffman_decoding(encoded_text, root):
+def huffman_decoding(encoded_text, root, output_file):
     decoded_text = ""
     current_node = root
+    error_flag = False
+
     for bit in encoded_text:
         if bit == '0':
             current_node = current_node.left
         elif bit == '1':
             current_node = current_node.right
+        else:
+            error_flag = True
+            break
 
-        if current_node is None:
-            raise ValueError("Invalid encoded sequence encountered at bit: " + bit)
+        if current_node is None or (current_node.char is None and bit not in ['0', '1']):
+            error_flag = True
+            break
 
         if current_node.char:
             decoded_text += current_node.char
-            current_node = root  # Reset to root after reaching a leaf node
+            current_node = root
+
+    if error_flag:
+        output_file.write("Error: Invalid encoded sequence encountered.\n")
+
     return decoded_text
