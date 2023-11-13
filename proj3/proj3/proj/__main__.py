f4b3c290b41a845618d9a8a077dc517074b843c4
AlexKShah <AlexKShah@users.noreply.github.com>
2023-11-13 11:02:00 -0500

    proj3 runner changes

--- proj3/proj3/proj/__main__.py
+++ proj3/proj3/proj/__main__.py
@@ -13,23 +13,24 @@ from proj.proj3 import huffman_process_files
 
 def main():
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("mode", choices=['encode', 'decode'], help="Operation mode: encode or decode")
     arg_parser.add_argument("frequency_table", type=str, help="Frequency Table Pathname")
-    arg_parser.add_argument("input_file", type=str, help="Input File Pathname")
-    arg_parser.add_argument("output_file", type=str, help="Output File Pathname")
+    arg_parser.add_argument("encoded_file", type=str, help="Encoded File Pathname")
+    arg_parser.add_argument("clear_file", type=str, help="Cleartext File Pathname")
+    arg_parser.add_argument("out_file", type=str, help="Output File Pathname")
     args = arg_parser.parse_args()
 
-    mode = args.mode
     frequency_table_path = args.frequency_table
-    input_file_path = args.input_file
-    output_file_path = args.output_file
+    encoded_file_path = args.encoded_file
+    clear_file_path = args.clear_file
+    output_file_path = args.out_file
 
     freq_path = Path(frequency_table_path)
-    input_path = Path(input_file_path)
+    encoded_path = Path(encoded_file_path)
+    clear_path = Path(clear_file_path)
     output_path = Path(output_file_path)
 
-    with freq_path.open('r') as freq_file, input_path.open('r') as input_file, output_path.open('w') as output_file:
-        huffman_process_files(freq_file, input_file, output_file, mode)
+    with freq_path.open('r') as freq_file, encoded_path.open('r') as encoded_file, clear_path.open('r') as clear_file, output_path.open('w') as output_file:
+        huffman_process_files(freq_file, encoded_file, clear_file, output_file)
 
 
 if __name__ == "__main__":
