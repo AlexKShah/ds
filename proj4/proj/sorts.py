64d7d0c7715fdca144a3da278a4d19fc9755d7c3
AlexKShah <AlexKShah@users.noreply.github.com>
2023-12-03 17:57:59 -0500

    update proj4

--- proj4/proj/sorts.py
+++ proj4/proj/sorts.py
@@ -6,70 +6,196 @@ __version__ = proj4
 """
 
 
-def quicksort_first_pivot(arr, low, high):
-    if low < high:
-        pi = partition(arr, low, high)
-        quicksort_first_pivot(arr, low, pi - 1)
-        quicksort_first_pivot(arr, pi + 1, high)
+class SortResult:
+    def __init__(self, sorted_data, comparisons, exchanges):
+        self.sorted_data = sorted_data
+        self.comparisons = comparisons
+        self.exchanges = exchanges
 
 
-def partition(arr, low, high):
-    pivot = arr[low]
-    i = low + 1
-    for j in range(low + 1, high + 1):
-        if arr[j] < pivot:
-            arr[i], arr[j] = arr[j], arr[i]
-            i += 1
-    arr[low], arr[i - 1] = arr[i - 1], arr[low]
-    return i - 1
-
-
-def quicksort_with_insertion(arr, low, high, threshold=100):
-    if high - low <= threshold:
-        insertion_sort(arr, low, high)
-    else:
-        if low < high:
-            pi = partition(arr, low, high)
-            quicksort_with_insertion(arr, low, pi - 1, threshold)
-            quicksort_with_insertion(arr, pi + 1, high, threshold)
-
-
-def insertion_sort(arr, low, high):
-    for i in range(low + 1, high + 1):
-        key = arr[i]
-        j = i - 1
-        while j >= low and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-
-
-def quicksort_median_pivot(arr, low, high):
-    if low < high:
-        pi = partition_median(arr, low, high)
-        quicksort_median_pivot(arr, low, pi - 1)
-        quicksort_median_pivot(arr, pi + 1, high)
-
-
-def partition_median(arr, low, high):
-    mid = (low + high) // 2
-    if arr[low] > arr[mid]:
-        arr[low], arr[mid] = arr[mid], arr[low]
-    if arr[low] > arr[high]:
-        arr[low], arr[high] = arr[high], arr[low]
-    if arr[mid] > arr[high]:
-        arr[mid], arr[high] = arr[high], arr[mid]
-    arr[mid], arr[high - 1] = arr[high - 1], arr[mid]
-    return partition(arr, low, high - 1)
-
-
-# Outline for Natural Merge Sort
 class ListNode:
     def __init__(self, value=0, next=None):
         self.value = value
         self.next = next
 
 
-def natural_merge_sort(head):
-    # todo
-    pass
+def quicksort_first_pivot(arr, low, high, comparisons=0, exchanges=0):
+    if low < high:
+        pivot_index, comp, exch = partition_first(arr, low, high)
+        comparisons += comp
+        exchanges += exch
+
+        arr, comp, exch = quicksort_first_pivot(arr, low, pivot_index - 1, comparisons, exchanges)
+        comparisons += comp
+        exchanges += exch
+
+        arr, comp, exch = quicksort_first_pivot(arr, pivot_index + 1, high, comparisons, exchanges)
+        comparisons += comp
+        exchanges += exch
+
+    return arr, comparisons, exchanges
+
+
+def partition_first(arr, low, high):
+    comparisons, exchanges = 0, 0
+    pivot = arr[low]
+    i = low + 1
+
+    for j in range(low + 1, high + 1):
+        comparisons += 1
+        if arr[j] < pivot:
+            arr[i], arr[j] = arr[j], arr[i]
+            exchanges += 1
+            i += 1
+
+    arr[low], arr[i - 1] = arr[i - 1], arr[low]
+    exchanges += 1
+    return i - 1, comparisons, exchanges
+
+
+def insertion_sort(arr, low, high):
+    comparisons, exchanges = 0, 0
+    for i in range(low + 1, high + 1):
+        key = arr[i]
+        j = i - 1
+
+        while j >= low:
+            comparisons += 1
+            if arr[j] > key:
+                arr[j + 1] = arr[j]
+                exchanges += 1
+                j -= 1
+            else:
+                break
+
+        arr[j + 1] = key
+        exchanges += 1
+
+    return arr, comparisons, exchanges
+
+
+def quicksort_insertion_100(arr, low, high, comparisons=0, exchanges=0):
+    if high - low <= 100:
+        arr, comp, exch = insertion_sort(arr, low, high)
+        return arr, comparisons + comp, exchanges + exch
+    else:
+        pivot_index, comp, exch = partition_first(arr, low, high)
+        comparisons += comp
+        exchanges += exch
+        arr, comp, exch = quicksort_insertion_100(arr, low, pivot_index - 1, comparisons, exchanges)
+        comparisons += comp
+        exchanges += exch
+        arr, comp, exch = quicksort_insertion_100(arr, pivot_index + 1, high, comparisons, exchanges)
+        comparisons += comp
+        exchanges += exch
+        return arr, comparisons, exchanges
+
+
+def quicksort_insertion_50(arr, low, high, comparisons=0, exchanges=0):
+    if high - low <= 50:
+        arr, comp, exch = insertion_sort(arr, low, high)
+        return arr, comparisons + comp, exchanges + exch
+    else:
+        pivot_index, comp, exch = partition_first(arr, low, high)
+        comparisons += comp
+        exchanges += exch
+        arr, comp, exch = quicksort_insertion_50(arr, low, pivot_index - 1, comparisons, exchanges)
+        comparisons += comp
+        exchanges += exch
+        arr, comp, exch = quicksort_insertion_50(arr, pivot_index + 1, high, comparisons, exchanges)
+        comparisons += comp
+        exchanges += exch
+        return arr, comparisons, exchanges
+
+
+def quicksort_median_pivot(arr, low, high, comparisons=0, exchanges=0):
+    if low < high:
+        pivot_index, comp, exch = partition_median(arr, low, high)
+        comparisons += comp
+        exchanges += exch
+        arr, comp, exch = quicksort_median_pivot(arr, low, pivot_index - 1, comparisons, exchanges)
+        comparisons += comp
+        exchanges += exch
+        arr, comp, exch = quicksort_median_pivot(arr, pivot_index + 1, high, comparisons, exchanges)
+        comparisons += comp
+        exchanges += exch
+    return arr, comparisons, exchanges
+
+
+def partition_median(arr, low, high):
+    comparisons, exchanges = 0, 0
+
+    # Finding the median of the first, middle, and last elements
+    mid = low + (high - low) // 2
+    if arr[mid] < arr[low]:
+        arr[mid], arr[low] = arr[low], arr[mid]
+        exchanges += 1
+    if arr[high] < arr[low]:
+        arr[high], arr[low] = arr[low], arr[high]
+        exchanges += 1
+    if arr[mid] < arr[high]:
+        arr[mid], arr[high] = arr[high], arr[mid]
+        exchanges += 1
+
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+        comparisons += 1
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+            exchanges += 1
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    exchanges += 1
+
+    return i + 1, comparisons, exchanges
+
+
+def natural_merge_sort(head, comparisons=0, exchanges=0):
+    if head is None or head.next is None:
+        return head, comparisons, exchanges
+
+    left_half, right_half = split_list(head)
+    left_sorted, left_comp, left_exch = natural_merge_sort(left_half)
+    right_sorted, right_comp, right_exch = natural_merge_sort(right_half)
+
+    merged, merge_comp, merge_exch = merge_sorted_lists(left_sorted, right_sorted)
+    total_comp = left_comp + right_comp + merge_comp
+    total_exch = left_exch + right_exch + merge_exch
+
+    return merged, total_comp, total_exch
+
+
+def split_list(head):
+    # Split the linked list into two halves
+    slow, fast = head, head.next
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    middle = slow.next
+    slow.next = None
+    return head, middle
+
+
+def merge_sorted_lists(left, right):
+    dummy = tail = ListNode()
+    comparisons, exchanges = 0, 0
+
+    while left and right:
+        comparisons += 1
+        if left.value < right.value:
+            tail.next = left
+            left = left.next
+        else:
+            tail.next = right
+            right = right.next
+        exchanges += 1
+        tail = tail.next
+
+    tail.next = left or right
+    exchanges += 1 if tail.next else 0
+
+    return dummy.next, comparisons, exchanges
--- proj4/proj/tests.py
+++ proj4/proj/tests.py
@@ -1,32 +1,81 @@
-from sorts import quicksort_first_pivot, quicksort_with_insertion, quicksort_median_pivot
 import random
-import time
+import os
+from sorts import *
 
 
-def generate_test_data(size, order='random'):
-    if order == 'random':
-        return [random.randint(1, 10000) for _ in range(size)]
-    elif order == 'reverse':
-        return list(range(size, 0, -1))
-    elif order == 'sorted':
-        return list(range(1, size + 1))
+def create_input_files(sizes):
+    os.makedirs('input_files', exist_ok=True)
+    for size in sizes:
+        # Random order
+        with open(f'input_files/input_random_{size}.txt', 'w') as file:
+            numbers = [random.randint(1, size * 100) for _ in range(size)]
+            file.write('\n'.join(map(str, numbers)))
+
+        # Ascending order
+        with open(f'input_files/input_ascending_{size}.txt', 'w') as file:
+            numbers = list(range(1, size + 1))
+            file.write('\n'.join(map(str, numbers)))
+
+        # Descending order
+        with open(f'input_files/input_descending_{size}.txt', 'w') as file:
+            numbers = list(range(size, 0, -1))
+            file.write('\n'.join(map(str, numbers)))
 
 
-def test_sort_function(sort_function, data):
-    start_time = time.time()
-    sort_function(data, 0, len(data) - 1)
-    end_time = time.time()
-    return end_time - start_time
+def array_to_linked_list(arr):
+    if not arr:
+        return None
+    head = tail = ListNode(arr[0])
+    for value in arr[1:]:
+        tail.next = ListNode(value)
+        tail = tail.next
+    return head
 
 
-def run_sorting_tests():
-    for size in [50, 1000, 2000, 5000, 10000]:
-        for order in ['random', 'reverse', 'sorted']:
-            data = generate_test_data(size, order)
-            time_taken = test_sort_function(quicksort_first_pivot, data.copy())
-            print(f"Quicksort (First Pivot) - Size: {size}, Order: {order}, Time: {time_taken:.4f}s")
-            # Repeat for other sorting functions and analyze performance
+def linked_list_to_array(head):
+    arr = []
+    current = head
+    while current:
+        arr.append(current.value)
+        current = current.next
+    return arr
+
+
+def run_sorts():
+    sizes = [50, 1000, 2000, 5000, 10000]
+    create_input_files(sizes)
+    sorting_functions = [
+        quicksort_first_pivot, quicksort_insertion_100, quicksort_insertion_50,
+        quicksort_median_pivot, natural_merge_sort
+    ]
+
+    for size in sizes:
+        for order in ['random', 'ascending', 'descending']:
+            file_path = f'input_files/input_{order}_{size}.txt'
+            with open(file_path, 'r') as file:
+                data = [int(line.strip()) for line in file]
+
+            for sort_function in sorting_functions:
+                try:
+                    copied_data = data.copy()
+
+                    if sort_function == natural_merge_sort:
+                        # Convert array to linked list for natural merge sort
+                        head = array_to_linked_list(copied_data)
+                        head, comparisons, exchanges = sort_function(head)
+                        sorted_data = linked_list_to_array(head)  # Convert back to array after sorting
+                    else:
+                        sorted_data, comparisons, exchanges = sort_function(copied_data, 0, len(copied_data) - 1)
+
+                    if size == 50:
+                        # Print sorted data and counts for size 50
+                        print(f"\n{sort_function.__name__}, {order}, Size {size}:")
+                        print("Sorted Data:", sorted_data)
+                    print(f"Comparisons: {comparisons}, Exchanges: {exchanges}")
+
+                except Exception as e:
+                    print(f"Error in {sort_function.__name__}, {order}, Size {size}: {e}")
 
 
 if __name__ == "__main__":
-    run_sorting_tests()
+    run_sorts()
