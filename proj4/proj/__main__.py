e36b6db19ca0a64bf4f7cd7fceebb6a796e60977
AlexKShah <AlexKShah@users.noreply.github.com>
2023-12-05 14:27:25 -0500

    Update __main__.py

--- proj4/proj/__main__.py
+++ proj4/proj/__main__.py
@@ -4,16 +4,17 @@ Initial run point for proj, requires no args for proj4
 __author__ = Alex Shah
 __version__ = proj4
 """
-from pathlib import Path
-import argparse
+
+# from pathlib import Path
+# import argparse
 from proj.tests import run_sorts
 
 
 def main():
-    arg_parser = argparse.ArgumentParser()
-    args = arg_parser.parse_args()
-
     run_sorts()
+
+    # arg_parser = argparse.ArgumentParser()
+    # args = arg_parser.parse_args()
 
 
 if __name__ == "__main__":
